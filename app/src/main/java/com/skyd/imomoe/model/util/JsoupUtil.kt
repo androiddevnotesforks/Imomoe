78954212ad72bd00be827d20b69dfbf4e2c44a00
MR.小迷糊 <1843025451@qq.com>
2022-02-24 00:24:54 +0800

    [feature]JsoupUtil类支持自定义MediaType编码 (#68)
    新增 自定义编码解析格式；新增 默认解析为GB2312字符编码

--- app/src/main/java/com/skyd/imomoe/model/util/JsoupUtil.kt
+++ app/src/main/java/com/skyd/imomoe/model/util/JsoupUtil.kt
@@ -4,6 +4,9 @@ import com.skyd.imomoe.config.Const
 import com.skyd.imomoe.net.RetrofitManager
 import com.skyd.imomoe.net.service.HtmlService
 import com.skyd.imomoe.util.Util.toEncodedUrl
+import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.ResponseBody.Companion.toResponseBody
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import kotlin.random.Random
@@ -30,5 +33,27 @@ object JsoupUtil {
                 Const.Request.USER_AGENT_ARRAY[Random.nextInt(Const.Request.USER_AGENT_ARRAY.size)]
             ).execute().body()?.string() ?: ""
         )
+    }
+
+    /**
+     * 指定解析类型
+     */
+    suspend fun getDocument(url: String, mediaType: MediaType): Document {
+        return runCatching {
+            Jsoup.parse(
+                RetrofitManager.get().create(HtmlService::class.java).getHtml(
+                    url.toEncodedUrl(),
+                    Const.Request.USER_AGENT_ARRAY[Random.nextInt(Const.Request.USER_AGENT_ARRAY.size)]
+                ).byteString().toResponseBody(mediaType).string()
+            )
+        }.getOrThrow()
+    }
+
+    /**
+     * 解析GB2312字符编码
+     */
+    suspend fun getDocumentByGB2312Type(url: String): Document {
+        val mediaType = "text/html; charset=gb2312".toMediaType()
+        return getDocument(url, mediaType)
     }
 }
